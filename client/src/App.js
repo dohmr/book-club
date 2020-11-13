e9e4ae406a1acb7d552e6c3eacd6b81cc6b4d614
doh <theshawns0n@gmail.com>
2020-11-13 10:23:00 -0500

    MVC

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<h1>BOOKS!!!! from GOOGLE</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ routes/books.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+const booksController = require("../../controllers/booksController");
+
+
+router.route("/")
+
+router.route("/books")
+
+router.route("/saved")
+
+router.route("/saved/:id")
