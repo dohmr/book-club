0a67d7b055c3090c2e929358957f8cd61730ca78
doh <theshawns0n@gmail.com>
2020-11-13 22:58:55 -0500

    controller/model/routes ++

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -3,7 +3,7 @@ const db = require("../models");
 module.exports = {
 
     findAll: function (req, res) {
-        db.Book
+        db.Books
             .find(req.query)
             .sort({ date: -1 })
             .then(dbModel => res.json(dbModel))
@@ -13,7 +13,7 @@ module.exports = {
             });
     },
     findById: function (req, res) {
-        db.Book
+        db.Books
             .findById(req.params.id)
             .then(dbModel => res.json(dbModel))
             .catch(err => {
@@ -22,7 +22,7 @@ module.exports = {
             });
     },
     create: function (req, res) {
-        db.Book
+        db.Books
             .create(req.body)
             .then(dbModel => res.json(dbModel))
             .catch(err => {
@@ -31,7 +31,7 @@ module.exports = {
             });
     },
     update: function (req, res) {
-        db.Book
+        db.Books
             .findOneAndUpdate({ _id: req.params.id }, req.body)
             .then(dbModel => res.json(dbModel))
             .catch(err => {
@@ -40,7 +40,7 @@ module.exports = {
             });
     },
     remove: function (req, res) {
-        db.Book
+        db.Books
             .findById({ _id: req.params.id })
             .then(dbModel => dbModel.remove())
             .then(dbModel => res.json(dbModel))
--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,8 @@ const router = require("express").Router();
 const bookRoutes = require("./books");
 
 router.use("/books", bookRoutes);
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 module.exports = router;
